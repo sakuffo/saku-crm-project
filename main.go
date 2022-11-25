48c434c3d3dda668ad4e4040e0e826a0c91416cc
Stephen Akuffo <sa.akuffo@gmail.com>
2022-11-24 20:27:10 -0500

    submission candidate

--- main.go
+++ main.go
@@ -20,6 +20,8 @@ type Customer struct {
 	Contacted   bool
 }
 
+// SEEDING DATA START
+
 var customer01 Customer = Customer{1, "Steve Kin", "IT Admin", "skin@admin.org", "677-787-1009", true}
 var customer02 Customer = Customer{2, "Nomuka Wil", "SRE", "nwil@admin.org", "647-787-1119", false}
 var customer03 Customer = Customer{3, "Kacie Oppa", "IT Admin", "kopp@admin.org", "457-755-1002", false}
@@ -34,20 +36,18 @@ var CUSTOMER_DB = map[string]Customer{
 	"5": customer05,
 }
 
-// CRUD Functions
+// SEEDING DATA END
 
-func getAllCustJSON(w http.ResponseWriter, r *http.Request) {
+func getCustomers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	for _, customer := range CUSTOMER_DB {
 		json.NewEncoder(w).Encode(customer)
 	}
 }
 
-func getCustJSON(w http.ResponseWriter, r *http.Request) {
+func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	id := mux.Vars(r)["id"]
-	fmt.Println(id)
-	fmt.Println(CUSTOMER_DB[id])
 	json.NewEncoder(w).Encode(CUSTOMER_DB[id])
 }
 
@@ -56,7 +56,7 @@ func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	if _, ok := CUSTOMER_DB[id]; ok {
 		delete(CUSTOMER_DB, id)
-		getAllCustJSON(w, r)
+		getCustomers(w, r)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "User Not Found")
@@ -75,7 +75,7 @@ func buildCustomerAddBeta(cust *Customer, json *map[string]string) {
 
 }
 
-func addCustomerBeta(w http.ResponseWriter, r *http.Request) {
+func addCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// How to Add a Customer
 
@@ -88,14 +88,13 @@ func addCustomerBeta(w http.ResponseWriter, r *http.Request) {
 	var tempCon Customer
 	buildCustomerAddBeta(&tempCon, &test)
 	strIndex := strconv.Itoa(len(CUSTOMER_DB) + 10)
-	CUSTOMER_DB[strIndex] = tempCon
 
 	if _, ok := CUSTOMER_DB[strIndex]; ok {
 		w.WriteHeader(http.StatusConflict)
 		fmt.Fprintf(w, "User Already exists")
 	} else {
 		CUSTOMER_DB[strIndex] = tempCon
-		getAllCustJSON(w, r)
+		getCustomers(w, r)
 	}
 }
 
@@ -110,23 +109,23 @@ func buildCustomerUpdateBeta(cust *Customer, json *map[string]string) {
 
 }
 
-func updateCustomerBeta(w http.ResponseWriter, r *http.Request) {
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	// How to Update a Customer
 
 	reqBody, err := io.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
+
 	var test map[string]string
 	json.Unmarshal(reqBody, &test)
-	strIndex, _ := test["Id"]
+	strIndex := test["Id"]
 	tempCon := CUSTOMER_DB[strIndex]
 	buildCustomerUpdateBeta(&tempCon, &test)
 
 	if _, ok := CUSTOMER_DB[strIndex]; ok {
 		CUSTOMER_DB[strIndex] = tempCon
-		getAllCustJSON(w, r)
+		getCustomers(w, r)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "User Not Found")
@@ -147,15 +146,14 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	// Handlers || CRUD
-	router.HandleFunc("/", index).Methods("GET") // Basic Implemented. Copy for instructions left []
+	router.HandleFunc("/", index).Methods("GET")
 
-	router.HandleFunc("/customers", addCustomerBeta).Methods("POST")     // Not implemented // CREATE
-	router.HandleFunc("/customers", getAllCustJSON).Methods("GET")       // READ Implemented. Uses a for loop to render the json
-	router.HandleFunc("/customers", updateCustomerBeta).Methods("PATCH") // Not Implemented // UPDATE
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers", updateCustomer).Methods("PATCH")
 
-	router.HandleFunc("/customers/{id}", updateCustomerBeta).Methods("PATCH")
-	router.HandleFunc("/customers/{id}", getCustJSON).Methods("GET")          // READ 1 Implemented
-	router.HandleFunc("/deleteCustomer/{id}", deleteCustomer).Methods("POST") // DELETE Implemented
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
 
 	fmt.Println("Server is starting on port 3300...")
 	log.Fatal(http.ListenAndServe(":3300", router))
